652604dc4457c59aa20d8d02b0381851b8d0d4c9
filipforsstrom <filipforsstrom@gmail.com>
2023-04-02 16:35:05 +0200

    simple link to github repo in window

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,12 @@ import { enable, isEnabled } from "tauri-plugin-autostart-api";
 
 enable().then(() => {});
 
-// 1. Select the div element using the id property
-const app = document.getElementById("app");
-// 2. Create a new <p></p> element programmatically
-const autostartIsEnabled = document.createElement("p");
-// 3. Add the text content
-
-autostartIsEnabled.textContent = "Hello, World!";
-
 isEnabled().then((enabled) => {
   console.log(enabled);
 });
 
-// 4. Append the p element to the div element
-app?.appendChild(autostartIsEnabled);
+function autoStartEnabled() {
+  isEnabled().then((enabled) => {
+    return enabled;
+  });
+}
