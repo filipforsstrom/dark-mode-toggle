a89eb01e97bfa4e16ec9c5ffb50e1d250a7e683f
filipforsstrom <filipforsstrom@gmail.com>
2023-04-02 16:44:54 +0200

    removed unsued function

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,3 @@ enable().then(() => {});
 isEnabled().then((enabled) => {
   console.log(enabled);
 });
-
-function autoStartEnabled() {
-  isEnabled().then((enabled) => {
-    return enabled;
-  });
-}
